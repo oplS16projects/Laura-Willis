79dad872cc89850138fcf32a5753e73ebc8d1d0b
Willis Hand <willishand@gmail.com>
2016-04-04 11:24:16 -0400

    basic game functionality

--- hangman.rkt
+++ hangman.rkt
@@ -7,15 +7,31 @@
 (define (new-game difficulty)
   (cond ((eq? difficulty 'easy) (make-game 'easy))
         ((eq? difficulty 'hard) (make-game 'hard))
-        (else (make-game 'normal (car (list-tail dictionary (random (length dictionary))))))))
+        (else (make-game 'normal (symbol->string (car (list-tail dictionary (random (length dictionary)))))))))
 
 ;; Game object, use constructor new-game
 (define (make-game difficulty wordlist)
   (define guesslist '())
+  (define word-in-progress (make-string (string-length wordlist) #\*))
   (define wrong-count 0)
   (define gameover #f)
-  (define (guess char) (+ 1 1))
+  (define (guess char) (if gameover (println "The game is over...")
+                           (cond ((eq? difficulty 'easy) (println "Not yet implemented")) ;easy implementation
+                             ((eq? difficulty 'hard) (println "Not yet implemented")) ;hard implementation
+                             (else (begin (set! guesslist (append guesslist '(char))) ;normal implementation
+                                          (if (string-contains? wordlist char)
+                                              (begin (println "play success sound")
+                                                     (println "Correct guess!"))
+                                              (begin (println "play failure sound")
+                                                     (set! wrong-count (+ wrong-count 1))
+                                                     (when (> wrong-count 6) (set! gameover #t)))))))))
   (define (dispatch msg)
     (cond ((eq? msg 'guess) guess)
           (else (error "Invalid dispatch request" msg))))
   dispatch)
+
+;; Starts a new game on normal difficulty, makes testing faster
+(define game (new-game 'normal))
+
+;; Wrapper for guessing function, makes testing faster
+(define (guess char) ((game 'guess) char))
