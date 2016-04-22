11877cce7c05b3a40877616740ba2ec169d7ff4e
Willis Hand <willishand@gmail.com>
2016-04-22 16:03:10 -0400

    added labels to gui

--- hangman.rkt
+++ hangman.rkt
@@ -15,6 +15,37 @@
 (define f (new frame% [label "Hang me, with sounds!"] [width 600] [height 600]))
 (define bm (make-object bitmap% (get-pure-port (string->url "https://raw.githubusercontent.com/oplS16projects/Laura-Willis/master/Deck.png"))))
 
+;; Loads the deck image into a 'canvas'
+(define pos -55)
+(define mycanvas%
+  (class canvas%
+    (super-new)
+    (inherit get-dc)
+         (define/override (on-paint)
+            (let ([my-dc (get-dc)])
+              (send my-dc draw-bitmap bm pos pos)))))
+
+;;
+;; IMPORTANT, below is code adding parts to the frame
+;;
+(define c (new mycanvas% [parent f]))
+
+(define wiplabel (new message% [parent f] [label ""] ))
+(define guesslabel (new message% [parent f] [label ""] ))
+(define infolabel (new message% [parent f] [label "Type easy, normal, or hard to begin a new game."] ))
+
+;; Event handler for text box
+(define (event-handler t e) (define boxinput (send t get-value))
+  (if (eqv? (send e get-event-type) 'text-field-enter)
+      (begin (send t set-value "") (cond ((string-ci=? boxinput "easy") (println "starteasygame"))
+                                         ((string-ci=? boxinput "normal") (set! game (new-game 'normal)))
+                                         ((string-ci=? boxinput "hard") (println "starthardgame"))
+                                         ((= 1 (string-length boxinput)) (guess boxinput))))
+      #f))
+;; Add a text field to the dialog
+(define txt (new text-field% [parent f] [label "Please enter a letter:"] [callback event-handler]))
+
+
 ;; Loads list of words in from file, will eventually excludes words outside of length limits
 (define (lengthcheck str)
   (if (or (< (string-length str) MINWORDLENGTH) (> (string-length str) MAXWORDLENGTH))
@@ -32,11 +63,12 @@
 
 ;; Game object, use constructor new-game
 (define (make-game difficulty wordlist)
-  (define guesslist '())
+  (define guesslist "")
   (define word-in-progress (make-string (string-length wordlist) #\*))
   (define wrong-count 0)
   (define gameover #f)
   (println word-in-progress)
+  (send wiplabel set-label word-in-progress)
   ;; Revealing letters that are guessed
   (define (reveal key char count)
     (cond ((= count (string-length word-in-progress)) #t) 
@@ -46,49 +78,21 @@
   (define (guess char) (if gameover (println "The game is over...")
                            (cond ((eq? difficulty 'easy) (println "Not yet implemented")) ;easy implementation
                              ((eq? difficulty 'hard) (println "Not yet implemented")) ;hard implementation
-                             ('else (begin (set! guesslist (append guesslist '(char))) ;normal implementation
+                             ('else (begin (set! guesslist (string-append guesslist char ", ")) ;normal implementation
                                           (if (string-contains? wordlist char)
                                               (begin (play SUCCESS)
                                                      (println "Correct guess!")
-                                                     (reveal wordlist (string-ref char 0) 0)
-                                                     (println word-in-progress)) ;update word-in-progress here, then print it
+                                                     (reveal wordlist (string-ref char 0) 0) ;update word-in-progress here, then print it
+                                                     (send wiplabel set-label word-in-progress)
+                                                     (send guesslabel set-label guesslist)) 
                                               (begin (play FAILURE)
                                                      (set! wrong-count (+ wrong-count 1))
-                                                     (when (> wrong-count 6) (set! gameover #t)))))))))
+                                                     (when (> wrong-count 6) (set! gameover #t))
+                                                     (send guesslabel set-label guesslist))))))))
   (define (dispatch msg)
     (cond ((eq? msg 'guess) guess)
           ('else (error "Invalid dispatch request" msg))))
   dispatch)
-
-;; Loads the deck image into a 'canvas'
-(define pos -55)
-(define mycanvas%
-  (class canvas%
-    (super-new)
-    (inherit get-dc)
-         (define/override (on-paint)
-            (let ([my-dc (get-dc)])
-              (send my-dc draw-bitmap bm pos pos)))))
-
-;;
-;; IMPORTANT, below is code adding parts to the frame
-;;
-(define c (new mycanvas% [parent f]))
-
-
-(define cclabel (new message% [parent f] [label "Type easy, normal, or hard to begin a new game."] ))
-
-;; Event handler for text box
-(define (event-handler t e) (define boxinput (send t get-value))
-  (if (eqv? (send e get-event-type) 'text-field-enter)
-      (begin (send t set-value "") (cond ((string-ci=? boxinput "easy") (println "starteasygame"))
-                                         ((string-ci=? boxinput "normal") (set! game (new-game 'normal)))
-                                         ((string-ci=? boxinput "hard") (println "starthardgame"))
-                                         ((= 1 (string-length boxinput)) (guess boxinput))))
-      #f))
-;; Add a text field to the dialog
-(define txt (new text-field% [parent f] [label "Please enter a letter:"] [callback event-handler]))
-
 
 ;; MAKES FRAME VISIBLE
 (send f show #t)
